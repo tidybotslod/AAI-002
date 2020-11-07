87b745e4e5580b40b4186b55107f313104e6cb92
XCLAIRLAPTOP+Craig <ctsinclair@live.com>
2020-11-07 08:17:42 -0800

    Second lab in the Cognitive series, requires the first lab to be downloaded

---
+++ UnitTests/UnitTest1.cs
@@ -0,0 +1,117 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.IO;
+using keySentiments;
+using System.Linq.Expressions;
+using System.Security.Cryptography;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        class TestCase
+        {
+            internal string[] input;
+            internal string[] answer;
+        }
+
+        private static TestCase[] testCases = {
+            new TestCase {
+                input  = new string[] { "The quick brown fox jumps over the lazy dog" },
+                answer = new string[] { "Negative, 0.00, 0.99, 0.01, \"quick brown fox jumps\", \"lazy dog\"" }
+            }
+        };
+
+        private void loadStream(Stream load, int testNumber)
+        {
+            StreamWriter writer = new StreamWriter(load, Console.OutputEncoding);
+            for (int line = 0; line < testCases[testNumber].input.Length; line++)
+            {
+                writer.WriteLine(testCases[testNumber].input[line]);
+                writer.Flush();
+                load.Seek(0, SeekOrigin.Begin);
+            }
+        }
+
+        private void checkOutput(Stream sink, int testNumber)
+        {
+            // Rewind the output, allows it to be read in.
+            sink.Seek(0, SeekOrigin.Begin);
+
+            // Read back the results line by line and compare them to what is expected.
+            StreamReader output = new StreamReader(sink, Console.InputEncoding);
+            for (int line = 0; line < testCases[testNumber].answer.Length; line++)
+            {
+                string value = output.ReadLine();
+                // Make sure the same value came back;
+                Assert.AreEqual(testCases[testNumber].answer[line], value);
+            }
+        }
+
+        [TestMethod]
+        [DataRow(0, DisplayName = "StreamLine1")]
+        public void testStreamData(int testNumber)
+        {
+            Program app = new Program();
+            using (MemoryStream source = new MemoryStream())
+            {
+                using (MemoryStream sink = new MemoryStream())
+                {
+                    // Write out the test case to a stream
+                    loadStream(source, testNumber);
+
+                    // Create a TextReader over the stream, data will be pulled here
+                    StreamReader reader = new StreamReader(source, Console.InputEncoding);
+
+                    // Create an output Stream to get the data
+                    StreamWriter result = new StreamWriter(sink, Console.OutputEncoding);
+
+                    // Get the results
+                    app.StreamData(reader, result);
+
+                    // Evaulate result
+                    checkOutput(sink, testNumber);
+                }
+            }
+        }
+
+        [TestMethod]
+        [DataRow(0, DisplayName = "PromptForLine1")]
+
+        public void testPromptForInput(int testNumber)
+        {
+            Program app = new Program();
+            using (MemoryStream source = new MemoryStream())
+            {
+                using (MemoryStream sink = new MemoryStream())
+                {
+                    try
+                    {
+                        // Write out the test case to a stream 
+                        loadStream(source, testNumber);
+
+                        // Create a TextReader over the memory stream and replace the input stream used by the console
+                        TextReader reader = new StreamReader(source, Console.InputEncoding);
+                        Console.SetIn(reader);
+
+                        // Create a Writer that will stream out the results
+                        StreamWriter result = new StreamWriter(sink, Console.OutputEncoding);
+
+                        // Get the results
+                        app.PromptForInput(result);
+
+                        // Test if the output lines match what is expected
+                        checkOutput(sink, testNumber);
+                    }
+                    finally
+                    {
+                        // Reset Console so it does not affect other tests.
+                        StreamReader reset = new StreamReader(Console.OpenStandardInput());
+                        Console.SetIn(reset);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ keySentiments/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.CommandLine;
+using System.CommandLine.Invocation;
+using System.IO;
+using AAI;
+
+namespace keySentiments
+{
+    public class Program
+    {
+        public void PromptForInput(StreamWriter writer)
+        {
+            KeySentiments server = new KeySentiments();
+            do
+            {
+                Console.Write("Enter text <empty to quit>: ");
+                string inputLine = Console.ReadLine();
+                if (inputLine == null || inputLine.Length == 0)
+                {
+                    break;
+                }
+                else
+                {
+                    server.Sentiment(inputLine, writer);
+                }
+            } while (true);
+        }
+
+        public void StreamData(StreamReader reader, StreamWriter writer)
+        {
+            KeySentiments server = new KeySentiments();
+            do
+            {
+                string inputLine = reader.ReadLine();
+                server.Sentiment(inputLine, writer);
+            } while (reader.EndOfStream == false);
+        }
+
+
+        public static void Main(string[] args)
+        {
+            var rootCommand = new RootCommand
+            {
+              new Option<string>(
+                    new string [] { "-i", "--inputFile" },
+                    description: "Optionally specify an input file, if no file is specified read from stdin"),
+              new Option<string>(
+                  new string [] { "-o", "--outputFile" },
+                  description: "Optionally specify an output file, if no fie is spedified write to stdout"),
+              new Option<bool>(
+                  new string [] { "-p", "--prompt" },
+                  description: "Optionally prompt for input from the command line")
+            };
+
+            rootCommand.Description = "Analysis text for key words or for sentiment.  This could be used to categorize the text and maintain the statics on how well a topic is preceived. Writes out the sentiment, positive score, negative score, neutral, and finally";
+
+            rootCommand.Handler = CommandHandler.Create<string, string, bool>((inputFile, outputFile, prompt) =>
+            {
+                StreamReader input = null;
+                StreamWriter output = null;
+
+                if (inputFile != null)
+                {
+                    input = File.OpenText(inputFile);
+                }
+
+                if (outputFile != null)
+                {
+                    output = File.AppendText(outputFile);
+                }
+                Program azureAccess = new Program();
+                if (output == null)
+                {
+                    output = new StreamWriter(Console.OpenStandardOutput(), Console.OutputEncoding);
+                }
+
+                if (prompt)
+                {
+                    azureAccess.PromptForInput(output);
+                }
+                else
+                {
+                    if (inputFile == null)
+                    {
+                        input = new StreamReader(Console.OpenStandardInput(), Console.InputEncoding);
+                    }
+                    azureAccess.StreamData(input, output);
+                }
+            });
+
+            rootCommand.InvokeAsync(args).Wait();
+        }
+    }
+}
